7c418ff10ff6e682447782680e504ac647c68fa1
Santeri Suomala <santeri.suomala@outlook.com>
2023-06-05 15:11:52 +0300

    not it

--- Casks/santerj-katana-driver.rb
+++ Casks/santerj-katana-driver.rb
@@ -11,13 +11,13 @@ cask "santerj-katana-driver" do
     sha256 "044ffb814fbef8cedf5ee5f3a62ac11da79124c3f8405e31da4fed07180fab25"
 
     pkg "KatanaUSBDriver/Katana_USBDriver11.pkg"
-    binary "/Applications/BOSS/KATANA Driver/KATANA Driver Setup.app/Contents/MacOS/KATANA Driver Setup", target: "KATANA Driver Setup"
 
     uninstall pkgutil: [
         "jp.co.roland.Katana.midi.11.pkg",
         "jp.co.roland.Katana.setup.11.pkg",
         "jp.co.roland.Katana.audio.11.pkg"
-    ]
+    ],
+        delete: "/Applications/BOSS"
 
     #app "KATANA Driver Setup.app"
 end
