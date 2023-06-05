0fd046816b88dc40e7af42c5fbff6ed63e20e9fb
Santeri Suomala <santeri.suomala@outlook.com>
2023-06-05 16:42:02 +0300

    remove sha256 checks

--- Casks/katana-driver.rb
+++ Casks/katana-driver.rb
@@ -1,5 +1,6 @@
 cask "katana-driver" do
     version "1.0.3"
+    sha256 :no_check
     
     name "BOSS Tone Studio for Katana"
     homepage "https://www.boss.info/uk/support/by_product/katana-50/updates_drivers/"
@@ -8,7 +9,6 @@ cask "katana-driver" do
 
     url "https://static.roland.com/assets/media/tgz/katana_mac11drv#{version.delete('.')}.tgz",
         verified: "static.roland.com"
-    sha256 "044ffb814fbef8cedf5ee5f3a62ac11da79124c3f8405e31da4fed07180fab25"
 
     pkg "KatanaUSBDriver/Katana_USBDriver11.pkg"
 
--- Casks/katana-tone-studio-mk1.rb
+++ Casks/katana-tone-studio-mk1.rb
@@ -1,15 +1,15 @@
 cask "katana-tone-studio-mk1" do
     version "4.0.0"
+    sha256 :no_check
 
     name "BOSS Tone Studio for Katana"
     homepage "https://www.boss.info/uk/support/by_product/katana-50/updates_drivers/"
     desc "Visual editor for BOSS Katana (Mk1) amplifiers"
-    
+
     depends_on cask: "katana-driver"
 
     url "https://static.roland.com/assets/media/zip/bts_katana_m#{version.delete('.')}.zip",
         verified: "static.roland.com"
-    sha256 "7e47013594f01025960ac3bd4e01d03328e38490547a02e5e3bd32289c965209"
 
     container nested: "bts_katana_m400/BOSS TONE STUDIO for KATANA.dmg"
 
