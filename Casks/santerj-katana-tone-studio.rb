a63dc075b217b36731afc453bb4b815a3f1d48f4
Santeri Suomala <santeri.suomala@outlook.com>
2023-06-05 13:15:58 +0300

    wip wip wip

--- Casks/santerj-katana-tone-studio.rb
+++ Casks/santerj-katana-tone-studio.rb
@@ -5,8 +5,11 @@ cask "santerj-katana-tone-studio" do
     homepage "https://www.boss.info/uk/support/by_product/katana-50/updates_drivers/"
     desc "Editor for Katana modeling amp family"
 
-    url "https://static.roland.com/assets/media/zip/bts_katana_m400.zip"
+    url "https://static.roland.com/assets/media/zip/bts_katana_m400.zip",
+        verified: "www.boss.info"
     sha256 "7e47013594f01025960ac3bd4e01d03328e38490547a02e5e3bd32289c965209"
+
+    container nested: "BOSS TONE STUDIO for KATANA.dmg"
 
     app "BOSS_TONE_STUDIO_for_KATANA.app"
   
